ToeBee/myjosm

commit c94a9be980b283beb05147c776e04c8ca19ab98b

Improves selection graphical performance by replacing the external calls to Component.getGraphics() with proper Swing paint mechanism
git-svn-id: https://josm.openstreetmap.de/svn/trunk@5500 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/NavigatableComponent.java
@@ -3,8 +3,11 @@ package org.openstreetmap.josm.gui;
 
 import static org.openstreetmap.josm.tools.I18n.marktr;
 
+import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
@@ -107,6 +110,10 @@ public class NavigatableComponent extends JComponent implements Helpful {
      */
     protected EastNorth center = calculateDefaultCenter();
 
+    private static final Object paintRequestLock = new Object();
+    private Rectangle paintRect = null;
+    private Polygon paintPoly = null;
+    
     public NavigatableComponent() {
         setLayout(null);
     }
@@ -1264,5 +1271,77 @@ public class NavigatableComponent extends JComponent implements Helpful {
             }
         }
         Cursors = c;
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        synchronized (paintRequestLock) {
+            if (paintRect != null) {
+                Graphics g2 = g.create();
+                g2.setColor(Color.BLACK);
+                g2.setXORMode(Color.WHITE);
+                g2.drawRect(paintRect.x, paintRect.y, paintRect.width, paintRect.height);
+            }
+            if (paintPoly != null) {
+                Graphics g2 = g.create();
+                g2.setColor(Color.WHITE);
+                g2.drawPolyline(paintPoly.xpoints, paintPoly.ypoints, paintPoly.npoints);
+            }
+        }
+        super.paint(g);
+    }
+
+    /**
+     * Requests to paint the given {@code Rectangle}.
+     * @param r The Rectangle to draw
+     * @see #requestClearRect
+     * @since 5500
+     */
+    public void requestPaintRect(Rectangle r) {
+        if (r != null) {
+            synchronized (paintRequestLock) {
+                paintRect = r;
+            }
+            repaint();
+        }
+    }
+    
+    /**
+     * Requests to paint the given {@code Polygon} as a polyline (unclosed polygon).
+     * @param p The Polygon to draw
+     * @see #requestClearPoly
+     * @since 5500
+     */
+    public void requestPaintPoly(Polygon p) {
+        if (p != null) {
+            synchronized (paintRequestLock) {
+                paintPoly = p;
+            }
+            repaint();
+        }
+    }
+    
+    /**
+     * Requests to clear the rectangled previously drawn.
+     * @see #requestPaintRect
+     * @since 5500
+     */
+    public void requestClearRect() {
+        synchronized (paintRequestLock) {
+            paintRect = null;
+        }
+        repaint();
+    }
+
+    /**
+     * Requests to clear the polyline previously drawn.
+     * @see #requestPaintPoly
+     * @since 5500
+     */
+    public void requestClearPoly() {
+        synchronized (paintRequestLock) {
+            paintPoly = null;
+        }
+        repaint();
     }
 }

src/org/openstreetmap/josm/gui/SelectionManager.java
@@ -1,9 +1,7 @@
 // License: GPL. Copyright 2007 by Immanuel Scholz and others
 package org.openstreetmap.josm.gui;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
@@ -56,9 +54,7 @@ public class SelectionManager implements MouseListener, MouseMotionListener, Pro
         /**
          * Called, when the left mouse button was released.
          * @param r The rectangle that is currently the selection.
-         * @param alt Whether the alt key was pressed
-         * @param shift Whether the shift key was pressed
-         * @param ctrl Whether the ctrl key was pressed
+         * @param e The mouse event.
          * @see InputEvent#getModifiersEx()
          */
         public void selectionEnded(Rectangle r, MouseEvent e);
@@ -118,6 +114,7 @@ public class SelectionManager implements MouseListener, MouseMotionListener, Pro
     /**
      * Register itself at the given event source.
      * @param eventSource The emitter of the mouse events.
+     * @param lassoMode {@code true} to enable lasso mode, {@code false} to disable it.
      */
     public void register(NavigatableComponent eventSource, boolean lassoMode) {
        this.lassoMode = lassoMode;
@@ -198,11 +195,12 @@ public class SelectionManager implements MouseListener, MouseMotionListener, Pro
         // disable the selection rect
         Rectangle r;
         if (!lassoMode) {
-            paintRect();
+            nc.requestClearRect();
             r = getSelectionRectangle();
 
             lasso = rectToPolygon(r);
         } else {
+            nc.requestClearPoly();
             lasso.addPoint(mousePos.x, mousePos.y);
             r = lasso.getBounds();
         }
@@ -215,33 +213,20 @@ public class SelectionManager implements MouseListener, MouseMotionListener, Pro
     }
 
     /**
-     * Draw a selection rectangle on screen. If already a rectangle is drawn,
-     * it is removed instead.
+     * Draws a selection rectangle on screen.
      */
     private void paintRect() {
         if (mousePos == null || mousePosStart == null || mousePos == mousePosStart)
             return;
-        Graphics g = nc.getGraphics();
-        g.setColor(Color.BLACK);
-        g.setXORMode(Color.WHITE);
-
-        Rectangle r = getSelectionRectangle();
-        g.drawRect(r.x,r.y,r.width,r.height);
+        nc.requestPaintRect(getSelectionRectangle());
     }
-
+    
     private void paintLasso() {
         if (mousePos == null || mousePosStart == null || mousePos == mousePosStart) {
             return;
         }
-
-        Graphics g = nc.getGraphics();
-        g.setColor(Color.WHITE);
-
-        int lastPosX = lasso.xpoints[lasso.npoints - 1];
-        int lastPosY = lasso.ypoints[lasso.npoints - 1];
-        g.drawLine(lastPosX, lastPosY, mousePos.x, mousePos.y);
-
         lasso.addPoint(mousePos.x, mousePos.y);
+        nc.requestPaintPoly(lasso);
     }
 
     /**
@@ -301,6 +286,7 @@ public class SelectionManager implements MouseListener, MouseMotionListener, Pro
      *
      * @param alt Whether the alt key was pressed, which means select all
      * objects that are touched, instead those which are completely covered.
+     * @return The collection of selected objects.
      */
     public Collection<OsmPrimitive> getSelectedObjects(boolean alt) {
 
@@ -367,6 +353,10 @@ public class SelectionManager implements MouseListener, MouseMotionListener, Pro
         return poly;
     }
 
+    /**
+     * Enables or disables the lasso mode.
+     * @param lassoMode {@code true} to enable lasso mode, {@code false} to disable it.
+     */
     public void setLassoMode(boolean lassoMode) {
         this.lassoMode = lassoMode;
     }

commit 0445c4ddad0ec43d499f1c728024dacc7d690834

Fix graphical glitch caused by setXORMode with Direct3D, introduced by r5500
git-svn-id: https://josm.openstreetmap.de/svn/trunk@5515 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/NavigatableComponent.java
@@ -41,12 +41,14 @@ import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.osm.WaySegment;
+import org.openstreetmap.josm.data.osm.visitor.paint.PaintColors;
 import org.openstreetmap.josm.data.preferences.IntegerProperty;
 import org.openstreetmap.josm.data.projection.Projection;
 import org.openstreetmap.josm.data.projection.Projections;
 import org.openstreetmap.josm.gui.help.Helpful;
 import org.openstreetmap.josm.gui.preferences.projection.ProjectionPreference;
 import org.openstreetmap.josm.tools.Predicate;
+import org.openstreetmap.josm.tools.Utils;
 
 /**
  * An component that can be navigated by a mapmover. Used as map view and for the
@@ -110,7 +112,7 @@ public class NavigatableComponent extends JComponent implements Helpful {
      */
     protected EastNorth center = calculateDefaultCenter();
 
-    private static final Object paintRequestLock = new Object();
+    private final Object paintRequestLock = new Object();
     private Rectangle paintRect = null;
     private Polygon paintPoly = null;
     
@@ -1278,14 +1280,15 @@ public class NavigatableComponent extends JComponent implements Helpful {
         synchronized (paintRequestLock) {
             if (paintRect != null) {
                 Graphics g2 = g.create();
-                g2.setColor(Color.BLACK);
-                g2.setXORMode(Color.WHITE);
+                g2.setColor(Utils.complement(PaintColors.getBackgroundColor()));
                 g2.drawRect(paintRect.x, paintRect.y, paintRect.width, paintRect.height);
+                g2.dispose();
             }
             if (paintPoly != null) {
                 Graphics g2 = g.create();
-                g2.setColor(Color.WHITE);
+                g2.setColor(Utils.complement(PaintColors.getBackgroundColor()));
                 g2.drawPolyline(paintPoly.xpoints, paintPoly.ypoints, paintPoly.npoints);
+                g2.dispose();
             }
         }
         super.paint(g);