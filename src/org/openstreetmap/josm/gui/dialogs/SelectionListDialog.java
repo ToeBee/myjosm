ToeBee/myjosm

commit 773b622971b8c3cf9b65f30954f0a69a7d8c2125

code cleanup / robustness in edit layer handling
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6336 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/SelectionListDialog.java
@@ -167,9 +167,10 @@ public class SelectionListDialog extends ToggleDialog  {
         DatasetEventManager.getInstance().addDatasetListener(model, FireMode.IN_EDT);
         MapView.addEditLayerChangeListener(actSearch);
         // editLayerChanged also gets the selection history of the level
-        model.editLayerChanged(null, Main.map.mapView.getEditLayer());
-        if (Main.map.mapView.getEditLayer() != null) {
-            model.setJOSMSelection(Main.map.mapView.getEditLayer().data.getAllSelected());
+        OsmDataLayer editLayer = Main.main.getEditLayer();
+        model.editLayerChanged(null, editLayer);
+        if (editLayer != null) {
+            model.setJOSMSelection(editLayer.data.getAllSelected());
         }
         actSearch.updateEnabledState();
     }
@@ -284,7 +285,7 @@ public class SelectionListDialog extends ToggleDialog  {
         }
 
         public void updateEnabledState() {
-            setEnabled(Main.main != null && Main.main.getEditLayer() != null);
+            setEnabled(Main.main != null && !Main.main.hasEditLayer());
         }
 
         @Override
@@ -309,8 +310,9 @@ public class SelectionListDialog extends ToggleDialog  {
         public void actionPerformed(ActionEvent e) {
             Collection<OsmPrimitive> sel = model.getSelected();
             if (sel.isEmpty())return;
-            if (Main.map == null || Main.map.mapView == null || Main.map.mapView.getEditLayer() == null) return;
-            Main.map.mapView.getEditLayer().data.setSelected(sel);
+            OsmDataLayer editLayer = Main.main.getEditLayer();
+            if (editLayer == null) return;
+            editLayer.data.setSelected(sel);
         }
 
         public void updateEnabledState() {

src/org/openstreetmap/josm/Main.java
@@ -543,8 +543,7 @@ abstract public class Main {
      * @return the current edit layer. <code>null</code>, if no current edit layer exists
      */
     public OsmDataLayer getEditLayer() {
-        if (map == null) return null;
-        if (map.mapView == null) return null;
+        if (!isDisplayingMapView()) return null;
         return map.mapView.getEditLayer();
     }
 
@@ -564,8 +563,7 @@ abstract public class Main {
      * @return the currently active layer. <code>null</code>, if currently no active layer exists
      */
     public Layer getActiveLayer() {
-        if (map == null) return null;
-        if (map.mapView == null) return null;
+        if (!isDisplayingMapView()) return null;
         return map.mapView.getActiveLayer();
     }
 
@@ -761,7 +759,7 @@ abstract public class Main {
      * @since 2025
      */
     public static boolean saveUnsavedModifications() {
-        if (map == null || map.mapView == null) return true;
+        if (!isDisplayingMapView()) return true;
         return saveUnsavedModifications(map.mapView.getLayersOfType(OsmDataLayer.class), true);
     }
 

src/org/openstreetmap/josm/actions/AutoScaleAction.java
@@ -61,8 +61,7 @@ public class AutoScaleAction extends JosmAction {
      *
      */
     public static void zoomToSelection() {
-        if (Main.main == null || Main.main.getEditLayer() == null) return;
-        if (Main.map == null || Main.map.mapView == null) return;
+        if (Main.main == null || !Main.main.hasEditLayer()) return;
         Collection<OsmPrimitive> sel = Main.main.getEditLayer().data.getSelected();
         if (sel.isEmpty()) {
             JOptionPane.showMessageDialog(

src/org/openstreetmap/josm/actions/CreateMultipolygonAction.java
@@ -66,7 +66,7 @@ public class CreateMultipolygonAction extends JosmAction {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (Main.main.getEditLayer() == null) {
+        if (!Main.main.hasEditLayer()) {
             new Notification(
                     tr("No data loaded."))
                     .setIcon(JOptionPane.WARNING_MESSAGE)

src/org/openstreetmap/josm/actions/DownloadReferrersAction.java
@@ -80,9 +80,9 @@ public class DownloadReferrersAction extends JosmAction{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (!isEnabled() || ! Main.isDisplayingMapView())
+        if (!isEnabled())
             return;
-        OsmDataLayer layer = Main.map.mapView.getEditLayer();
+        OsmDataLayer layer = Main.main.getEditLayer();
         if (layer == null)
             return;
         Collection<OsmPrimitive> primitives = layer.data.getSelected();

src/org/openstreetmap/josm/actions/InfoAction.java
@@ -1,4 +1,4 @@
-//License: GPL. Copyright 2007 by Immanuel Scholz and others
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.actions;
 
 import java.awt.event.ActionEvent;
@@ -16,6 +16,9 @@ import org.openstreetmap.josm.tools.Shortcut;
 
 public class InfoAction extends JosmAction {
 
+    /**
+     * Constructs a new {@code InfoAction}.
+     */
     public InfoAction() {
         super(tr("Advanced info"), "about",
             tr("Display advanced object information about OSM nodes, ways, or relations."),
@@ -29,7 +32,7 @@ public class InfoAction extends JosmAction {
     public void actionPerformed(ActionEvent ae) {
         DataSet set = getCurrentDataSet();
         if (set != null) {
-            new InspectPrimitiveDialog(set.getAllSelected(), Main.map.mapView.getEditLayer()).showDialog();
+            new InspectPrimitiveDialog(set.getAllSelected(), Main.main.getEditLayer()).showDialog();
         }
     }
 

src/org/openstreetmap/josm/actions/JoinAreasAction.java
@@ -518,7 +518,7 @@ public class JoinAreasAction extends JosmAction {
 
         // Delete the discarded inner ways
         if (!discardedWays.isEmpty()) {
-            Command deleteCmd = DeleteCommand.delete(Main.map.mapView.getEditLayer(), discardedWays, true);
+            Command deleteCmd = DeleteCommand.delete(Main.main.getEditLayer(), discardedWays, true);
             if (deleteCmd != null) {
                 cmds.add(deleteCmd);
                 commitCommands(marktr("Delete Ways that are not part of an inner multipolygon"));
@@ -834,7 +834,7 @@ public class JoinAreasAction extends JosmAction {
         List<List<Node>> chunks = buildNodeChunks(way, nodes);
 
         if (chunks.size() > 1) {
-            SplitWayResult split = SplitWayAction.splitWay(Main.map.mapView.getEditLayer(), way, chunks, Collections.<OsmPrimitive>emptyList());
+            SplitWayResult split = SplitWayAction.splitWay(Main.main.getEditLayer(), way, chunks, Collections.<OsmPrimitive>emptyList());
 
             //execute the command, we need the results
             cmds.add(split.getCommand());

src/org/openstreetmap/josm/actions/PasteAction.java
@@ -130,7 +130,7 @@ public final class PasteAction extends JosmAction implements PasteBufferChangedL
         for (PrimitiveData data:bufferCopy) {
             if (data instanceof NodeData) {
                 NodeData nodeData = (NodeData)data;
-                if (Main.map.mapView.getEditLayer() == source) {
+                if (Main.main.getEditLayer() == source) {
                     nodeData.setEastNorth(nodeData.getEastNorth().add(offsetEast, offsetNorth));
                 }
             } else if (data instanceof WayData) {

commit 4b5de9cdc538d137fe549b694d67ec03fef71beb

fix #9251 - Search button disabled - regression from r6336
git-svn-id: https://josm.openstreetmap.de/svn/trunk@6353 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/gui/dialogs/SelectionListDialog.java
@@ -76,7 +76,7 @@ import org.openstreetmap.josm.tools.SubclassFilteredCollection;
 
 /**
  * A small tool dialog for displaying the current selection.
- *
+ * @since 8
  */
 public class SelectionListDialog extends ToggleDialog  {
     private JList lstPrimitives;
@@ -285,7 +285,7 @@ public class SelectionListDialog extends ToggleDialog  {
         }
 
         public void updateEnabledState() {
-            setEnabled(Main.main != null && !Main.main.hasEditLayer());
+            setEnabled(Main.main != null && Main.main.hasEditLayer());
         }
 
         @Override